3687ed524b155a4790fdf523d18237460abe1794
EC2 Default User <ec2-user@ip-172-31-4-251.ap-northeast-1.compute.internal>
2022-11-03 13:00:59 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,53 @@
 class BooksController < ApplicationController
+  def top
+    @books = Book.index
+  end
+  
   def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+  if @book.save
+    flash[:notice] = 'successfully'
+    redirect_to book_path(@book.id)
+  else
+    render('books/new')
+  end
   end
 
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+  if @book.update(book_params)
+      flash[:notice] = 'successfully'
+      redirect_to book_path(@book.id)
+  else
+    render('books/edit')
+  end
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'  
+  end
+  
+   private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- db/migrate/20221014163619_create_lists.rb
+++
@@ -1,9 +0,0 @@
-class CreateLists < ActiveRecord::Migration[6.1]
-  def change
-    create_table :lists do |t|
-      t.string :title
-      t.string :body
-      t.timestamps
-    end
-  end
-end
